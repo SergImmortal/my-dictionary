ee6bf1e6b382404ebc2d6820915eaaff3b299471
Sergey Maksimenko <s.maxymenko@gmail.com>
2021-06-24 20:52:42 +0300

    Added random qoute with cache in session storage

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './article.css'
 import Btn from "../btn/btn";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+
+
 
 const Article = () => {
+    const [quote, setQuote] = useState(null);
+    const [author, setAuthor] = useState(null);
+    const [quoteIsLoaded, setQuoteIsLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!window.sessionStorage.getItem("quote")) {
+            if (!quoteIsLoaded) {
+                fetch('https://api.quotable.io/random')
+                    .then(data => data.json())
+                    .then(data => {
+                        setQuoteIsLoaded(true);
+                        setAuthor(data.author);
+                        setQuote(data.content);
+                        window.sessionStorage.setItem("quote", JSON.stringify(
+                            {
+                                quoute: data.content,
+                                author: data.author
+                            }
+                        ));
+                        console.log(data);
+                    }, []);
+            }
+        } else {
+            const dataSet = JSON.parse(window.sessionStorage.getItem("quote"));
+            setQuoteIsLoaded(true);
+            setAuthor(dataSet.author);
+            setQuote(dataSet.quoute);
+        }
+    });
+
     return (
         <div className='article'>
-            <q>Your favorite quote</q>
+            {quoteIsLoaded && <q>{quote} &#169; {author}</q>}
             <div className='wrap'>
                 <Link to="/info" className='link'>
-                    <Btn label='MORE ENGLISH'/>
+                    <Btn label='MORE ENGLISH' />
                 </Link>
                 <Link to="/dictionary" className='link effect1'>
                     LET'S GO!
